670b070e4bf5c437e4514053c2f43dba928a4e94
Parth Kolekar <parth.kolekar@students.iiit.ac.in>
2015-02-08 17:05:15 +0530

    Adding particles collider effects.

---
+++ src/Particles.cpp
@@ -0,0 +1,14 @@
+#ifndef _PARTICLES_CPP
+#define _PARTICLES_CPP 
+
+#include "Particles.h"
+
+Particles::Particles() {
+
+}
+
+Particles::~Particles() {
+	
+}
+
+#endif
---
+++ src/Particles.h
@@ -0,0 +1,14 @@
+#ifndef _PARTICLES_H
+#define _PARTICLES_H 
+
+#include "GenericObject.h"
+
+class Particles : GenericObject
+{
+public:
+	Particles();
+	~Particles();
+	
+};
+
+#endif
